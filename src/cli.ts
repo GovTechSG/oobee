3cc982b00d717f387a27e27aa907e2ee51fdf798
Wilson WeeSheng Khoo <wilson.khooweesheng@cognizant.com>
2024-12-18 15:31:38 +0800

    Prettier linting changes

--- src/cli.ts
+++ src/cli.ts
@@ -184,11 +184,10 @@ Usage: npm run cli -- -c <crawler> -d <device> -w <viewport> -u <url> OPTIONS`,
     return option;
   })
   .check(argvs => {
-    if (
-      (argvs.scanner === ScannerTypes.CUSTOM) &&
-      argvs.maxpages
-    ) {
-      throw new Error('-p or --maxpages is only available in website, sitemap and local file scans.');
+    if (argvs.scanner === ScannerTypes.CUSTOM && argvs.maxpages) {
+      throw new Error(
+        '-p or --maxpages is only available in website, sitemap and local file scans.',
+      );
     }
     return true;
   })
