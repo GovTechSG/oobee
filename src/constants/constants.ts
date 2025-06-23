da1df32176bbb4bc4818348d1a2be55cd42a06b2
Sachin <sachin.gamage@cognizant.com>
2025-06-23 18:13:53 +0800

    Adding crashpad location for cloud deployments.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -228,6 +228,7 @@ if (fs.existsSync('/.dockerenv')) {
     '--no-sandbox',
     '--disable-dev-shm-usage',
     '--homedir=/tmp/oobee/home',
+    '--crash-dump-directory=/tmp/oobee/crashpad'
   ];
 }
 
