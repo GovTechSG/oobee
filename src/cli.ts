a19b88e33ffc61b31b70e9fa99d91d537a3c08c2
younglim <younglim@users.noreply.github.com>
2025-08-25 15:46:51 +0800

    Exit from scan if error code is met

--- src/cli.ts
+++ src/cli.ts
@@ -252,16 +252,19 @@ const scanInit = async (argvs: Answers): Promise<string> => {
       printMessage([statuses.unauthorised.message], messageOptions);
       consoleLogger.info(statuses.unauthorised.message);
       cleanUpAndExit(res.status);
+      return;
     }
     case statuses.cannotBeResolved.code: {
       printMessage([statuses.cannotBeResolved.message], messageOptions);
       consoleLogger.info(statuses.cannotBeResolved.message);
       cleanUpAndExit(res.status);
+      return;
     }
     case statuses.systemError.code: {
       printMessage([statuses.systemError.message], messageOptions);
       consoleLogger.info(statuses.systemError.message);
       cleanUpAndExit(res.status);
+      return;
     }
     case statuses.invalidUrl.code: {
       if (
@@ -291,25 +294,28 @@ const scanInit = async (argvs: Answers): Promise<string> => {
         consoleLogger.info(statuses.notASitemap.message);
         cleanUpAndExit(statuses.notASitemap.code);
       }
-      break;
+      return;
     }
     case statuses.notASitemap.code: {
       printMessage([statuses.notASitemap.message], messageOptions);
       consoleLogger.info(statuses.notASitemap.message);
       cleanUpAndExit(res.status);
+      return;
     }
     case statuses.notALocalFile.code: {
       printMessage([statuses.notALocalFile.message], messageOptions);
       consoleLogger.info(statuses.notALocalFile.message);
       cleanUpAndExit(res.status);
+      return;
     }
     case statuses.browserError.code: {
       printMessage([statuses.browserError.message], messageOptions);
       consoleLogger.info(statuses.browserError.message);
       cleanUpAndExit(res.status);
+      return;
     }
     default:
-      break;
+      return;
   }
 
   if (process.env.OOBEE_VERBOSE) {
