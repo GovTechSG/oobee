a771f55da2fb31eed82079c1772ae4bf1b442e7e
Zui Young <younglim@users.noreply.github.com>
2026-07-09 17:50:02 +0800

    Add support for telemetry

--- src/combine.ts
+++ src/combine.ts
@@ -18,7 +18,7 @@ import {
   getS3UploadPrefix,
   uploadFolderToS3,
 } from './services/s3Uploader.js';
-import { writeManifest, resetCaptureEntries } from './crawlers/pageCapture.js';
+import { writeManifest, resetCaptureEntries, isPageCaptureEnabled } from './crawlers/pageCapture.js';
 
 // Class exports
 export class ViewportSettingsClass {
@@ -75,6 +75,10 @@ const combineRun = async (details: Data, deviceToScan: string) => {
   process.env.CRAWLEE_LOG_LEVEL = 'ERROR';
   process.env.CRAWLEE_STORAGE_DIR = randomToken;
   constants.sitemapFetchedLinks = null;
+
+  if (isPageCaptureEnabled() && !process.env.OOBEE_SCAN_PRODUCT) {
+    process.env.OOBEE_SCAN_PRODUCT = 'U&A';
+  }
 
   if (process.env.CRAWLEE_SYSTEM_INFO_V2 === undefined) {
     // Set the environment variable to enable system info v2
